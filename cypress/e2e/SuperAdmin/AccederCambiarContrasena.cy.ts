3af7449cead2309c8cf6571b93a0b127bad7cb3b
Santiago Boggetti <santiago@gux.tech>
2024-09-09 18:50:04 -0300

    Add files via upload

---
+++ cypress/e2e/SuperAdmin/AccederCambiarContrasena.cy.ts
@@ -0,0 +1,47 @@
+/// <reference types="cypress"/>
+describe('Test Suite', () => {
+
+  beforeEach(() => {
+    // Limpiar cookies y almacenamiento local antes de cada test
+    cy.clearCookies();
+    cy.clearLocalStorage();
+    cy.viewport(1440, 900);
+    cy.visit('https://endonet-app-staging.web.app/');
+
+    // Verificar si existe el botón de cerrar y hacer clic en él si está presente
+    cy.get('body').then(($body) => {
+      if ($body.find('.close').length > 0) {
+        cy.get('.close').click({ force: true });
+      }
+    });
+  });
+  
+  it('Login Correcto ingresando con Mail', () => {
+    
+cy.get('.classic-card').click();
+cy.get('#email').click();
+cy.get('#email').type('msanz@gux.tech');
+cy.get('#password1').click();
+cy.get('#password1').type('sB1HZ$iiwB6Ota');
+cy.get('.classic-card').click();
+cy.get('.mdc-button__label > span').click();
+cy.get('.mt-8').submit();
+cy.wait(1200);
+
+cy.get(':nth-child(6) > .fuse-vertical-navigation-item-wrapper > .mat-mdc-tooltip-trigger').click();
+cy.get('.text-4xl').should('contain.text', 'Ajustes');
+cy.get('.px-6 > :nth-child(2)').click();
+
+cy.get('#security > .ml-3').click();
+cy.get('.ml-2').should('contain.text', 'Seguridad');
+cy.get('#mat-mdc-form-field-label-10 > .ng-tns-c3736059725-11').should('be.visible');
+cy.get('#mat-mdc-form-field-label-12 > .ng-tns-c3736059725-12').should('be.visible');
+cy.get('mat-label.ng-tns-c3736059725-13').should('be.visible');
+
+
+
+  });
+  
+  
+
+});
---
+++ cypress/e2e/SuperAdmin/OpcionesBarraLateral.cy.ts
@@ -0,0 +1,42 @@
+/// <reference types="cypress"/>
+describe('Test Suite', () => {
+
+  beforeEach(() => {
+    // Limpiar cookies y almacenamiento local antes de cada test
+    cy.clearCookies();
+    cy.clearLocalStorage();
+    cy.viewport(1440, 900);
+    cy.visit('https://endonet-app-staging.web.app/');
+
+    // Verificar si existe el botón de cerrar y hacer clic en él si está presente
+    cy.get('body').then(($body) => {
+      if ($body.find('.close').length > 0) {
+        cy.get('.close').click({ force: true });
+      }
+    });
+  });
+  
+  it('Verificar que la página cargue correctamente', () => {
+    
+cy.get('.classic-card').click();
+cy.get('#email').click();
+cy.get('#email').type('msanz@gux.tech');
+cy.get('#password1').click();
+cy.get('#password1').type('sB1HZ$iiwB6Ota');
+cy.get('.classic-card').click();
+cy.get('.mdc-button__label > span').click();
+cy.get('.mt-8').submit();
+cy.wait(1200);
+cy.url().should('contains', 'https://endonet-app-staging.web.app/admin/dashboard/');
+cy.get('.px-6').should('be.visible');
+cy.get(':nth-child(2) > .fuse-vertical-navigation-item-wrapper > .mat-mdc-tooltip-trigger').should('be.visible');
+cy.get(':nth-child(3) > .fuse-vertical-navigation-item-wrapper > .mat-mdc-tooltip-trigger').should('be.visible');
+cy.get(':nth-child(4) > .fuse-vertical-navigation-item-wrapper > .mat-mdc-tooltip-trigger').should('be.visible');
+cy.get(':nth-child(5) > .fuse-vertical-navigation-item-wrapper > .mat-mdc-tooltip-trigger').should('be.visible');
+cy.get(':nth-child(5) > .fuse-vertical-navigation-item-wrapper > .mat-mdc-tooltip-trigger').should('be.visible');
+cy.get(':nth-child(6) > .fuse-vertical-navigation-item-wrapper > .mat-mdc-tooltip-trigger').should('be.visible');
+  });
+  
+  
+
+});
